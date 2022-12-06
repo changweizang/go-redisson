13672ef72e05a62c92cfe46a4fdfbb0ae9f97813
zangchangwei <zangchangwei@qianxin.com>
2022-12-06 09:55:04 +0800

    可重入

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/Tracy-4869/go-redisson
 go 1.17
 
 require (
+	gitee.com/zhouxiaozhu/go-delayqueue v0.0.2 // indirect
+	github.com/go-basic/uuid v1.0.0 // indirect
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 )
--- lock.go
+++ lock.go
@@ -1,3 +1,137 @@
 package go_redisson
 
+import (
+	"errors"
+	"fmt"
+	"gitee.com/zhouxiaozhu/go-delayqueue"
+	"github.com/go-redis/redis"
+	"github.com/google/uuid"
+	"time"
+)
+
+var WatchDogTimeout = 30 * time.Second
+var PublishMessage = 1
+
+var rLockScript = redis.NewScript(`
+-- 若锁不存在，新增锁、设置锁重入次数为1、设置锁过期时间
+if (redis.call('exists', 'KEYS[1]' == 0) then
+	redis.call('hincrby', KEYS[1], ARGV[2], 1);
+	redis.call('hexpire', KEYS[1], ARGV[1]);
+	return -1;
+end;
+-- 若锁存在且锁标识匹配，则表明当前加锁为可重入锁，将锁重入次数+1，并在此设置锁过期时间
+if (redis.call('hexists', KEYS[1], ARGV[2]) == 1) then
+	redis.call('hincrby', KEYS[1], ARGV[2], 1);
+	redis.call('hexpire', KEYS[1], ARGV[1]);
+	return -1;
+end; 
+-- 若锁存在但锁标识不匹配，则表明当前锁被占用，直接返回过期时间
+return redis.call('pttl', KEYS[1]);
+`)
+
+var rUnlockScript = redis.NewScript(`
+-- 判断当前锁是否还是被自己持有
+if (redis.call('hexists', KEYS[1], ARGV[1]) == 0) then
+	return 0;
+end;
+-- 判断可重入次数
+local count = redis.call('hincrby', KEYS[1], ARGV[1], -1);
+if (count > 0) then
+	redis.call('pexpire', KEYS[1], ARGV[2]);
+	return 1;
+else 
+	redis.call('del', KEYS[1]);
+	redis.call('publish', KEYS[2], ARGV[3]);
+	return 2;
+end;
+return 0;
+`)
+
+type Rlock struct {
+	Key string
+	rdb *redis.Client
+	uuid string
+}
+
+func InitRLock(key string, client *redis.Client) *Rlock {
+	return &Rlock{
+		Key: key,
+		rdb: client,
+		uuid: uuid.New().String(),
+	}
+}
+
+func (l *Rlock) TryLock(wTime int) error {
+	waitTime := int64(wTime * 1000)
+	current := time.Now().UnixNano() / 1e6
+	ttl, err := l.tryAcquire(-1)
+	if err != nil {
+		return err
+	}
+	// 获取锁成功
+	if ttl < 0 {
+		return nil
+	}
+	// 获取锁失败，尝试再次获取
+	waitTime -= time.Now().UnixNano() / 1e6 - current
+	if waitTime <= 0 {
+		return errors.New("get lock failed: time running out")
+	}
+	// 等待消息
+	current = time.Now().UnixNano() / 1e6
+
+	// todo
+	return nil
+}
+
+func (l *Rlock) tryAcquire(leaseTime int64) (int64, error) {
+	if leaseTime != -1 {
+		return l.tryAcquireInner(leaseTime)
+	}
+	// 默认使用看门狗的过期时间
+	ttl, err := l.tryAcquireInner(WatchDogTimeout.Milliseconds())
+	if err != nil {
+		return 0, err
+	}
+	if ttl < 0 {
+		// 获取锁成功， 对锁的有效期进行续约
+
+	}
+	return ttl, nil
+}
+
+func (l *Rlock) tryAcquireInner(leaseTime int64) (int64, error) {
+	gid := delayqueue.GetGoroutineID()
+	lockHashKey := fmt.Sprintf("%s:%d", l.uuid, gid)
+	result, err := rLockScript.Run(l.rdb, []string{l.Key}, leaseTime, lockHashKey).Result()
+	if err != nil {
+		return 0, err
+	}
+	return result.(int64), nil
+}
+
+func (l *Rlock) UnLock() error {
+	gid := delayqueue.GetGoroutineID()
+	publishChannel := fmt.Sprintf("publish-lock-channel:%s", l.Key)
+	lockHashKey := fmt.Sprintf("%s:%d", l.uuid, gid)
+	_, err := rUnlockScript.Run(l.rdb, []string{l.Key, publishChannel}, lockHashKey, WatchDogTimeout, PublishMessage).Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
